a56b61e50bf93d065c240211e40fab9674df4ece
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2021-08-18 03:01:37 +0300

    Модификация swapi-service. Теперь данные с сервера трансформируются в нужный формат и поля

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -16,28 +16,74 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResourse(`/people`);
-    return res.results;
+    return res.results.map(this._transformPerson);
   };
   async getPerson(id) {
-    return this.getResourse(`/people/${id}/`);
+    const person = this.getResourse(`/people/${id}/`);
+    return this._transformPerson(person);
   };
 
 
   async getAllPlanets() {
     const res = await this.getResourse(`/planets`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   };
   async getPlanet(id) {
-    return this.getResourse(`/planets/${id}/`);
+    const planet = await this.getResourse(`/planets/${id}/`);
+    return this._transformPlanet(planet);
   };
 
 
   async getAllStarships() {
     const res = await this.getResourse(`/starships`);
-    return res.results;
+    return res.results.map(this._transformStarship);
   };
   async getStarship(id) {
-    return this.getResourse(`/starships/${id}/`);
+    const starship = this.getResourse(`/starships/${id}/`);
+    return this._transformStarship(starship);
   };
+
+
+  // id вычленяется из url-адреса
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  // странсформация данных, которые получены от API
+  // вместо всех полей сохраняются только нужные
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    };
+  };
+
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    };
+  };
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
+  }
 
 };
